0ee1fed53154de2d01b6a0215357f1c4474aea53
Rafal Pych <r.pych27@gmail.com>
2020-03-29 17:06:23 +0200

    second init

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function(){
     $("button").click(function(){
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data){
 
-                var container = $('<div class="container"></div>')
+                var container = $('<div id="dane-programisty"></div>')
                 container.append('<h1>'+data.imie+'</h1>')
                 container.append('<h1>'+data.nazwisko+'</h1>')
                 container.append('<h1>'+data.zawod+'</h1>')
