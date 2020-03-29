3c62fc186f684294504addfa5d785c22f2d065a9
Rafal Pych <r.pych27@gmail.com>
2020-03-29 17:03:37 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+    $("button").click(function(){
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data){
+
+                var container = $('<div class="container"></div>')
+                container.append('<h1>'+data.imie+'</h1>')
+                container.append('<h1>'+data.nazwisko+'</h1>')
+                container.append('<h1>'+data.zawod+'</h1>')
+                container.append('<h1>'+data.firma+'</h1>')
+
+                $('.programmer-data').append(container);
+                
+             });
+
+    })          
+});
